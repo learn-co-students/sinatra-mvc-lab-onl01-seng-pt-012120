d9f1f79f2b0a7008d045eb1ebcc52e588016f94e
Roberto Livi <robertomlivi@outlook.com>
2020-04-10 00:33:14 +0000

    Done.

--- app.rb
+++ app.rb
@@ -13,8 +13,3 @@ class App < Sinatra::Base
   end
   
 end
-
-# "onceway uponway away imetay andway away eryvay oodgay imetay itway asway erethay asway away oocowmay omingcay ownday alongway ethay oadray andway isthay oocowmay atthay asway omingcay ownday alongway ethay oadray etmay away icenay ittlelay oybay amednay abybay uckootay"
-
-
-# "<!doctype html>\n<html>\n  \n<head>\n  <title>Piglatinized</title>\n  \n</head>\n\n<body>\n\nonceway...y ethay oadray etmay away icenay ittlelay oybay amednay abybay uckootay  \n</body>\n  \n  \n</html>" to include "Onceway uponway away imetay andway away eryvay oodgay imetay itway asway erethay aswayaway oocowmay...sway omingcay ownday alongway ethay oadray etmay away icenay ittlelay oybay amednay abybay uckootay"
