d0bb40669aa40df3a85b008526d7962dcb3d33fa
Jennifer Beaver <jen.lee.beaver@gmail.com>
2020-04-16 17:13:14 +0000

    Done.

--- app.rb
+++ app.rb
@@ -5,5 +5,11 @@ class App < Sinatra::Base
   get '/' do
     erb :user_input
   end
-  
+
+  post '/piglatinize' do
+    pl = PigLatinizer.new
+    @piglatin = pl.piglatinize(params[:user_phrase])
+    erb :results
+  end
+
 end
