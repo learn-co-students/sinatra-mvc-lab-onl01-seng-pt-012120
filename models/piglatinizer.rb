84684592894279b58cec494981ab22fc3ff6ad95
Daniel Sun <danielsun12345@gmail.com>
2020-04-16 22:35:04 +0000

    Done.

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,18 +1,23 @@
+require 'pry'
+
 class PigLatinizer
   attr_accessor :piglatinized_phrase
 
-  def piglatinize(word) #pig latinizes a single word
+  def piglatinize(word) 
+    word_array = word.split(" ")
+    word_array.collect {|w| piglatinize_word(w)}.join(" ")
+  end
+
+  def piglatinize_word(word)
     if word.downcase.index(/[aeiou]/) == 0
       word + "way"
     else
       vowel_index = word.index(/[aeiou]/)
-      front_end = word.slice!(0..vowel_index-1)
-      word + front_end +"ay"
+      front_end = word.slice(0..vowel_index-1)
+      back_end = word.slice(vowel_index..word.length-1)
+      back_end + front_end + "ay"
     end
   end
-
-  def to_pig_latin(phrase)
-    word_array = phrase.split(" ")
-    @piglatinized_phrase = word_array.collect {|word| piglatinize(word)}.join(" ")
-  end
+  
+  
 end 
