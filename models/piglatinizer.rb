76d08ac2277a465611cbb39480d7ee5b3766f7bb
James Batsell <jamesbatsell05@gmail.com>
2020-04-21 00:00:50 +0000

    Done.

---
+++ models/piglatinizer.rb
@@ -0,0 +1,25 @@
+  
+class PigLatinizer
+
+  attr_accessor :user_phrase
+
+  def to_pig_latin(word)
+    if word.downcase.index(/[aeiou]/) == 0
+      word + "way"
+    else
+      first_vowel_index = word.index(/[aeiou]/)
+      front = word.slice!(0..first_vowel_index-1)
+      word + front + "ay"
+    end
+  end
+
+  def piglatinize(phrase)
+    word_array = phrase.to_s.split(' ')
+    @user_phrase = word_array.collect{|word| to_pig_latin(word)}.join(" ")
+  end
+
+
+
+
+
+end
